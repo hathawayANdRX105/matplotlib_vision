073ab9003aa585b17b54841f46952297350750c3
hathawayANdRX105 <2635254302@qq.com>
2022-04-20 23:57:33 +0800

    pyecharts:finished d's part with 2 picture

--- util/parser.py
+++ util/parser.py
@@ -8,112 +8,112 @@ from wordcloud import WordCloud
 
 
 def setChinese():
-    plt.rcParams['font.family'] = ['FangSong']
-    # plt.rcParams['font.family'] = ['SimHei']
-    plt.rcParams['axes.unicode_minus'] = False
+	plt.rcParams['font.family'] = ['FangSong']
+	# plt.rcParams['font.family'] = ['SimHei']
+	plt.rcParams['axes.unicode_minus'] = False
 
 
 # word cloud
 def get_word_list(word_list_path):
-    f = open(word_list_path, encoding='utf-8')
-    wordList = f.read()
-    return wordList
+	f = open(word_list_path, encoding='utf-8')
+	wordList = f.read()
+	return wordList
 
 
 def get_word_cloud(word_list_path, img_mask_path):
-    word_list = get_word_list(word_list_path)
-    img_mask = np.array(image.open(img_mask_path))
+	word_list = get_word_list(word_list_path)
+	img_mask = np.array(image.open(img_mask_path))
 
-    # load wordlist and set background image mask
-    font_path = r"C:\Windows\Fonts\simhei.ttf"
-    wordcloud = WordCloud(font_path=font_path, background_color='white', mask=img_mask).generate(word_list)
+	# load wordlist and set background image mask
+	font_path = r"C:\Windows\Fonts\simhei.ttf"
+	wordcloud = WordCloud(font_path=font_path, background_color='white', mask=img_mask).generate(word_list)
 
-    plt.axis('off')  # undisplay axies
-    plt.imshow(wordcloud)
+	plt.axis('off')  # undisplay axies
+	plt.imshow(wordcloud)
 
 
 def get_specified_len_word_cloud(specified_data, specified_count, mask_path):
-    word_join = ""
-    all_list = []
+	word_join = ""
+	all_list = []
 
-    for word, count in zip(specified_data, specified_count):
-        temp_list = []
-        temp_list.append(word)
+	for word, count in zip(specified_data, specified_count):
+		temp_list = []
+		temp_list.append(word)
 
-        temp_list = temp_list * count
+		temp_list = temp_list * count
 
-        all_list += temp_list
+		all_list += temp_list
 
 
 def get_filter_source_symbols(source):
-    r_en = "[A-Za-z0-9_.!+-=——,$%^~@#￥%……&*《》<>「」{}()/\\\[\]'\"]"
-    filter_en_source = re.sub(r_en, '', source)
-    r_cn = r"[，。？ \n\t：！“”（）【】、；]"
-    filter_cn_source = re.sub(r_cn, '', filter_en_source)
+	r_en = "[A-Za-z0-9_.!+-=——,$%^~@#￥%……&*《》<>「」{}()/\\\[\]'\"]"
+	filter_en_source = re.sub(r_en, '', source)
+	r_cn = r"[，。？ \n\t：！“”（）【】、；]"
+	filter_cn_source = re.sub(r_cn, '', filter_en_source)
 
-    return filter_cn_source
+	return filter_cn_source
 
 
 def get_sort_data(data, data_count):
-    arg_index = data_count.argsort()
+	arg_index = data_count.argsort()
 
-    sort_data = data[arg_index]
-    sort_count = data_count[arg_index]
-    return sort_data, sort_count
+	sort_data = data[arg_index]
+	sort_count = data_count[arg_index]
+	return sort_data, sort_count
 
 
 def get_data_without_single_word(sort_result, sort_count):
-    i = 0
-    single_word_indexs = []
-    for word in sort_result:
-        if len(word) <= 1:
-            single_word_indexs.append(i)
+	i = 0
+	single_word_indexs = []
+	for word in sort_result:
+		if len(word) <= 1:
+			single_word_indexs.append(i)
 
-        i += 1
+		i += 1
 
-    data = np.delete(sort_result, single_word_indexs)
-    data_count = np.delete(sort_count, single_word_indexs)
+	data = np.delete(sort_result, single_word_indexs)
+	data_count = np.delete(sort_count, single_word_indexs)
 
-    return data, data_count
+	return data, data_count
 
 
 def get_data(word_list_path, extra_filter_func=None):
-    source = get_word_list(word_list_path)
+	source = get_word_list(word_list_path)
 
-    # 更多的筛选文本的补充函数, 输入文本, 输出过滤后的文本
-    if extra_filter_func:
-        source = extra_filter_func(source)
+	# 更多的筛选文本的补充函数, 输入文本, 输出过滤后的文本
+	if extra_filter_func:
+		source = extra_filter_func(source)
 
-    # print(filter_source)
-    split_word_list = list(jieba.cut(source, cut_all=False))
-    result, count = np.unique(split_word_list, return_counts=True)
+	# print(filter_source)
+	split_word_list = list(jieba.cut(source, cut_all=False))
+	result, count = np.unique(split_word_list, return_counts=True)
 
-    # 过滤 单个字符以及字符
-    data, data_count = get_data_without_single_word(result, count)
+	# 过滤 单个字符以及字符
+	data, data_count = get_data_without_single_word(result, count)
 
-    sort_data, sort_data_count = get_sort_data(data, data_count)
+	sort_data, sort_data_count = get_sort_data(data, data_count)
 
-    return sort_data, sort_data_count
+	return sort_data, sort_data_count
 
 
 def get_top_data(topNum, data, data_count):
-    size = len(data)
-    from_index = size - topNum
+	size = len(data)
+	from_index = size - topNum
 
-    return data[from_index:], data_count[from_index:]
+	return data[from_index:], data_count[from_index:]
 
 
 def getSpecifiedLenWord(slen, data, count):
-    unsatisfiedIndexs = []
-    i = 0
+	unsatisfiedIndexs = []
+	i = 0
 
-    for word in data:
-        if len(word) != slen:
-            unsatisfiedIndexs.append(i)
+	for word in data:
+		if len(word) != slen:
+			unsatisfiedIndexs.append(i)
 
-        i = i + 1
+		i = i + 1
 
-    data = np.delete(data, unsatisfiedIndexs)
-    count = np.delete(count, unsatisfiedIndexs)
+	data = np.delete(data, unsatisfiedIndexs)
+	count = np.delete(count, unsatisfiedIndexs)
 
-    return data, count
+	return data, count
